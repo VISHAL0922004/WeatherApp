169bdd2479e6dc8d22fb059f3675b95cafb73e4b
Vishal Rohilla <vishurohilla092004@hmail.com>
2025-10-18 22:49:26 +0530

    add this mistake

--- src/aoo.jsx
+++ src/aoo.jsx
@@ -1,7 +1,10 @@
 export default function App() {
 
-    return <div>Hello Vite + React!   world</div>;
+    return <div>Hello Vite + React!   world
+        <h1>mistake</h1>
+    </div>;
 
     return <div>Hello Vite + React</div>;
+    
 
 }
